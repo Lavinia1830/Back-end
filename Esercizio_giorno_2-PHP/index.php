b88fa1140dd2d570372663e1481f4715795cc86d
Lavinia1830 <lavinia.baratti@gmail.com>
2024-02-02 15:08:33 +0100

    Esercizio settimana 1 giorno 2 PHP

--- Esercizio_giorno_2-PHP/index.php
+++ Esercizio_giorno_2-PHP/index.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+
+    //print_r($_SESSION);
+    $contacts = [];
+    if(isset($_SESSION['contacts'])){
+        $contacts = $_SESSION['contacts'];
+    }
+    
+    
+    session_write_close();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,31 +23,69 @@
 </head>
 <body>
     <div class="container">
-        <form method="POST" action="gestione.php">
-            <div class="mb-3">
-                <label for="exampleInputName" class="form-label">Name</label>
-                <input type="text" class="form-control" id="exampleInputName" name='name'>
+        <form method="post" action="gestione.php" enctype="multipart/form-data" class="my-3">
+            <div class="row g-3">
+                <div class="col-sm">
+                    <input type="text" class="form-control" placeholder="Firstname..." name="firstname">
+                </div>
+                <div class="col-sm">
+                    <input type="text" class="form-control" placeholder="Lastname..." name="lastname">
+                </div>
+                <div class="col-sm">
+                    <input type="text" class="form-control" placeholder="City..." name="city">
+                </div>
+                <div class="col-sm">
+                    <input type="tel" class="form-control" placeholder="Phone..." name="phone">
+                </div>
+                <div class="col-sm">
+                    <input type="email" class="form-control" placeholder="Email..." name="email">
+                </div>
+                <div class="col-sm">
+                    <input type="file" class="form-control" placeholder="Image..." name="image">
+                </div>
+                <div class="col-sm">
+                    <button type="submit" class="btn btn-dark">Add Contact</button>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputSurname" class="form-label">Surname</label>
-                <input type="text" class="form-control" id="exampleInputSurname" name="surname">
-            </div>
-            <!-- <div class="mb-3">
-                <label for="exampleInputCity" class="form-label">City</label>
-                <input type="text" class="form-control" id="exampleInputCity" name="city">
-            </div> -->
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        <hr/>
+        <div class="my-3">
+            <table class="table table-dark table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Firstname</th>
+                        <th scope="col">Lastname</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    if($contacts){
+                    foreach ($contacts as $key => $contact) { 
+                    ?>
+                        <tr>
+                            <th scope="row"><?= $key+1 ?></th>
+                            <td><img src=<?= $contact['Image'] ?> width="50" ></td>
+                            <td><?= $contact['Firstname'] ?></td>
+                            <td><?= $contact['Lastname'] ?></td>
+                            <td><?= $contact['City'] ?></td>
+                            <td><?= $contact['Phone'] ?></td>
+                            <td><?= $contact['Email'] ?></td>
+                        </tr>
+                    <?php } }?>
+                </tbody>
+            </table>
+        </div>
     </div>
     
         
-        <ul>
-            <?php for($i=0; $i < 10; $i++){
-                session_start(); 
-                echo '<li> User: ' . $_SESSION['user'] . '</li>';
-                session_write_close();
-            }?>
-        </ul>
+    
+        
+        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
