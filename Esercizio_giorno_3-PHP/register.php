581315a2616e772453dfe1a61bf41986b8b882a6
Lavinia1830 <lavinia.baratti@gmail.com>
2024-02-02 17:08:18 +0100

    Esercizio settimana 1 giorno 3 PHP

--- Esercizio_giorno_3-PHP/register.php
+++ Esercizio_giorno_3-PHP/register.php
@@ -1,17 +1,3 @@
-<?php
-    session_start();
-
-    //print_r($_SESSION);
-    $contacts = [];
-    if(isset($_SESSION['contacts'])){
-        $contacts = $_SESSION['contacts'];
-    }
-    
-    
-    session_write_close();
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,11 +13,11 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#">Logo</a>
+                <a class="navbar-brand" href="index.php">Logo</a>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                     <a class="nav-link" href="#">about</a>
@@ -48,29 +34,23 @@
             </div>
         </nav>
     </heder>
-    <main class="container my-3">
-        <table class="table table-info table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Password</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                if($contacts){
-                foreach ($contacts as $key => $contact) { 
-                ?>
-                    <tr>
-                        <th scope="row"><?= $key+1 ?></th>
-                        <td><?= $contact['Email'] ?></td>
-                        <td><?= $contact['Password'] ?></td>
-                    </tr>
-                <?php } }?>
-                </tbody>
-        </table>
+    <main class="container">
+        <h1>Register</h1>
+        <form method="post" action="gestione.php" enctype="multipart/form-data" class="my-3">
+            <div class="row g-3">
+                <div class="row-sm">
+                    <input type="email" class="form-control" placeholder="Email..." name="email">
+                </div>
+                <div class="row-sm">
+                    <input type="password" class="form-control" placeholder="Password..." name="password">
+                </div>
+                <div class="row-sm">
+                    <button type="submit" name="register" class="btn btn-dark">Register</button>
+                </div>
+            </div>
+        </form>
     </main>
+    
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
