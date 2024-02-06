e7c0fa2c69324c31146f72bc6403c4d662322761
Lavinia1830 <lavinia.baratti@gmail.com>
2024-02-06 18:14:01 +0100

    Esercizio settimana 1 giorno 4 SQL

--- Esercizio_giorno_4-SQL/index.php
+++ Esercizio_giorno_4-SQL/index.php
@@ -1,44 +1,63 @@
 <?php
-    //Riciamo il file config
-    require_once 'config.php';
-    //var_dump($config); // mostra quello che hai scritto nell'array $config 
+    /* session_start();
 
-    $mysqli = new mysqli(
-        $config['mysql_host'],
-        $config['mysql_user'],
-        $config['mysql_password'],
-    );
-    if ($mysqli->connect_error) {
-        die($mysqli->connect_error);
+    //print_r($_SESSION);
+    $contacts = [];
+    if(isset($_SESSION['contacts'])){
+        $contacts = $_SESSION['contacts'];
     }
+    
+    session_write_close(); */
 
-    //Creo un DataBase
-    $sql = 'SELECT * FROM users';
-    $result = $mysqli->query($sql);
+    require_once 'config.php';
+    //var_dump($mysqli);
+    
+    include_once('function.php'); 
+    include_once('header.php'); 
+    include_once('navbar.php'); 
+
+    $contacts = getAllUser($mysqli);
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Lista utenti</title>
-</head>
-<body>
-    <h1>Lista Utenti</h1>
-    <p><a href="">Aggiungere Utente</a></p>
-    <table class="table-info table-striped">
-        
-    </table>
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+    <div class="container my-3">
+        <h1 class="text-center">Rubrica App</h1>
+        <div class="my-3">
+            <table class="table table-dark table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Firstname</th>
+                        <th scope="col">Lastname</th>
+                        <th scope="col">City</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    if($contacts){
+                    foreach ($contacts as $key => $contact) { 
+                    ?>
+                        <tr>
+                            <th scope="row"><?= $key+1 ?></th>
+                            <td><img src=<?= $contact['image'] ?> width="50" ></td>
+                            <td><?= $contact['firstname'] ?></td>
+                            <td><?= $contact['lastname'] ?></td>
+                            <td><?= $contact['city'] ?></td>
+                            <td><?= $contact['phone'] ?></td>
+                            <td><?= $contact['email'] ?></td>
+                            <td>
+                                <a class="btn btn-danger" href="controller.php?action=delete&id=<?= $contact['id'] ?>" role="button">X</a>
+                                <a class="btn btn-warning" href="update.php?id=<?= $contact['id'] ?>" role="button">M</a>
+                            </td>
+                        </tr>
+                    <?php } }?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+<?php include_once('footer.php'); ?>
