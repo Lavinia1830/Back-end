d8560fb83320abb6bd2e1f770740c6a0b0fd29b4
Lavinia1830 <lavinia.baratti@gmail.com>
2024-03-01 16:36:26 +0100

    Esercizio settimana 5 giorno 2

--- Esercizio_settimana5_giorno2-PHP/index.php
+++ Esercizio_settimana5_giorno2-PHP/index.php
@@ -1,16 +1,15 @@
 <?php
-    session_start();
+  session_start();
+  if(!isset($_SESSION['userLogin']) && isset($_COOKIE["useremail"]) && isset($_COOKIE["userpassword"])) {
+    header('Location: http://localhost/Back-end/Esercizio_settimana5_giorno2-PHP/controller.php?email='.$_COOKIE["useremail"].'&password='.$_COOKIE["userpassword"]);
+  } else if(!isset($_SESSION['userLogin'])) {
+    header('Location: http://localhost/Back-end/Esercizio_settimana5_giorno2-PHP/login.php');
+  } 
 
-    // Verifica se l'utente è autenticato
-    if(!isset($_SESSION['userLogin']) && isset($_COOKIE["useremail"]) && isset($_COOKIE["userpassword"])) {
-        header('Location: http://localhost/Back-end/Esercizio_settimana5_giorno2-PHP/controller.php?email='.$_COOKIE["useremail"].'&password='.$_COOKIE["userpassword"]);
-      } else if(!isset($_SESSION['userLogin'])) {
-        //print_r($_SESSION['userLogin']);
-        header('Location: http://localhost/Back-end/Esercizio_settimana5_giorno2-PHP/login.php');
-      } else 
+  if(!isset($_SESSION['content'])){
+    header('Location: http://localhost/Back-end/Esercizio_settimana5_giorno2-PHP/language.php?lang=it');
+  }
 
-    // Ottieni l'username dall'array $_COOKIE
-    $username = $_COOKIE['username'];
 ?>
 
 <!DOCTYPE html>
@@ -24,25 +23,48 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar w/ text</a>
+          <a class="navbar-brand" href="#">App PHP</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
               </li>
             </ul>
-            <span class="navbar-text">
-                <!-- <a class="nav-link active" aria-current="page" href="login.html">Login</a> -->
-                <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+            <span class="navbar-text d-flex">
+                <span class="me-3 d-flex">
+                  <a class="nav-link active" aria-current="page" href="language.php?lang=it">
+                    <img src="img/it.png" style="width:16px" class="mx-1" />
+                  </a>
+                  <a class="nav-link active" aria-current="page" href="language.php?lang=us">
+                    <img src="img/us.png" style="width:16px" class="mx-1" />
+                  </a>
+                </span>
+                <span>
+                  <!-- <a class="nav-link active" aria-current="page" href="login.html">Login</a> -->
+                  <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+                </span>
             </span>
           </div>
         </div>
     </nav>
     <div class="container">
-        <h1>Welcome, <?php echo $username; ?>!</h1>
+        <!-- <h1 class="text-center">Home Page</h1> -->
+        <h1 class="text-center"><?= $_SESSION['content']['title']  ?></h1>
+        <div class="card p-3">
+          <img src="<?= $_SESSION['content']['img']  ?>" />
+          <div class="my-3"><?= $_SESSION['content']['content']  ?></div>
+        </div>
+        <figure class="text-end">
+          <blockquote class="blockquote">
+            <p><?= $_SESSION['content']['author']  ?></p>
+          </blockquote>
+          <figcaption class="blockquote-footer">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+      </figure>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
